ba97dd7f4b19b97d04428ba046bc4cfddea2f63e
Marcos Vieira <marcosvieira482@gmail.com>
2024-05-20 21:22:26 -0300

    feat: add and configure Unsplash API to search images

--- app/src/App.js
+++ app/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import React from 'react';
 import './App.css';
 import SearchPhotos from './components/SearchPhoto';
@@ -6,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <div className='container'></div>
-      <div className='title'>Photo App</div>
+      <h1 className='title'>Photo App</h1>
       <SearchPhotos />
     </div>
   );
--- app/src/components/SearchPhoto.js
+++ app/src/components/SearchPhoto.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import Unsplash, { toJson } from "unsplash-js";
+//require('dotenv').config()
+
+const unsplash = new Unsplash({
+    accessKey: process.env.ACCESS_KEY
+})
 
 export default function SearchPhotos() {
+    const [query, setQuery] = useState("");
+    const [pics, setPics] = useState([]);
+
+    const searchP = async (e) => {
+        e.preventDefault();
+
+        unsplash.search.photos(query).then(toJson).then((json) => {
+            setPics(json.results);
+        });
+    }
+
     return (
         <div>
-            <form className="form">
+            <form className="form" onSubmit={searchP}>
                 <label htmlFor="query" className="label">
                     {""}
                 </label>
@@ -12,9 +29,22 @@ export default function SearchPhotos() {
                     type="text"
                     name="query"
                     className="input"
-                    placeholder={`Digite algo`} />
+                    placeholder={`Digite algo`}
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)} />
                 <button className="button" type="submit">Buscar</button>
             </form>
+            <div className="card-list">
+                {pics.map((pic) => <div className="card" key={pic.id}>
+                    <img
+                        src={pic.urls.full}
+                        alt={pic.alt_description}
+                        className="card-image"
+                        width="50%"
+                        height="50%">
+                    </img>
+                </div>)}
+            </div>
         </div>
     )
 }
